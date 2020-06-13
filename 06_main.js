a29c80f2852da7e8648a77a584946ce71ef0cf7e
frankthetankhub <61543714+frankthetankhub@users.noreply.github.com>
2020-06-13 23:54:00 +0200

    Update 06_main.js
    link fix

--- 06_main.js
+++ 06_main.js
@@ -26,7 +26,7 @@ $("document").ready(function() {
         // Here, you can specify all information for the deployment
         deploy: {
             experimentID: "109",
-            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/109/",
+            serverAppURL: "mental-rotation.netlify.app"//"https://magpie-demo.herokuapp.com/api/submit_experiment/109/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
